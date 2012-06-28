9e1e57145353631c4afec63e249199897e8943fa
Shae Erisson <shae@hopi>
2012-06-28 12:11:44 -0500

    add Blaze Markup instance for Display class

--- src/Network/Web/HyperHaskell/Display.hs
+++ src/Network/Web/HyperHaskell/Display.hs
@@ -24,12 +24,12 @@ instance ToJSON DisplayResult where
                         "clientType" .= clientType dr,
                         "resources" .= resources dr
                        ]
-  
+
 
 class Display a where
     display :: (Display a) => a -> DisplayResult
 
-displayEmpty = DisplayResult { clientType = "text", resources = [], result = "no result" }    
+displayEmpty = DisplayResult { clientType = "text", resources = [], result = "no result" }
 -- svg output
 -- svg    :: LiveResult -> LiveResult
 -- svg lr = lr {clientType = "SVG" }
@@ -39,11 +39,14 @@ displayEmpty = DisplayResult { clientType = "text", resources = [], result = "no
 -- renderDia SVG (SVGOptions "output.file" (Dims 200 200)) (circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0 :: Diagram Diagrams.Backend.SVG.SVG R2)
 renderMyDiagramToSvg dia = renderHtml $ renderDia SVG (SVGOptions "output.file" (Dims 200 200)) (dia :: Diagram Diagrams.Backend.SVG.SVG R2)
 
-instance Display (Diagram SVG R2) where 
+instance Display (Diagram SVG R2) where
     display d = DisplayResult { clientType="svg", result=renderMyDiagramToSvg d, resources = []}
-    
+
 instance Display Text where
     display d = DisplayResult { clientType="text", result= d, resources = []}
-    
+
 instance Display String where
     display d = display $ pack d -- pack to Data.Text
+
+instance Display Markup where
+    display d = display $ renderHtml d
